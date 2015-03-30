a2c33b882f0b7923eddabac945813f597a32549b
Joshua Piccari <joshua.piccari@gmail.com>
2015-03-29 21:32:09 -0700

    Update _.isNaN implementation to conform to isNaN spec.

--- lodash.src.js
+++ lodash.src.js
@@ -8823,7 +8823,8 @@
     function isNaN(value) {
       // An `NaN` primitive is the only value that is not equal to itself.
       // Perform the `toStringTag` check first to avoid errors with some host objects in IE.
-      return isNumber(value) && value != +value;
+      value = +value;
+      return value !== value;
     }
 
     /**
